a75de3d773ba18fd588e8562d14a3b381e186419
Pere <pere.solaclaver@gmail.com>
2019-09-18 13:02:04 +0100

    Install uuid in the right folder and create skeleton form

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import uuid from 'uuid';
+import Form from './Components/Form';
+import List from './Components/List';
 
 function App() {
   return (
     <div className="App">
-      
+      <Form />
+      <List />
     </div>
   );
 }
---
+++ team-builder/src/Components/Form.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Form () {
+    return (
+        <form>
+            <label>Name
+                <input type='text' />
+            </label>
+            <label>Position
+                <input type='text' />
+            </label>
+            <label>Name
+                <input type='text' />
+            </label>
+        </form>
+
+    )
+}
